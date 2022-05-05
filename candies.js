cc1652e2f9e5bf62478faf6dc2dcbce9a91eaa2c
Chad Stabler <stablerpsn@gmail.com>
2022-05-05 12:38:34 -0500

    built candy array to import and render

---
+++ candies.js
@@ -0,0 +1,5 @@
+export const candy = [
+    { name: 'jolly-ranchers', consistency: 'hard', flavorScore: '895', image: './assets/jolly-rancher.jpg' },
+    { name: 'life-savers-gummy', consistency: 'chewy', flavorScore: '600', image: './assets/life-savers-gummy.jpg' },
+    { name: 'cookie-dough-bites', consistency: 'chewy', flavorScore: '1000', image: './assets/cookie-dough-bites.jpg' } 
+];
